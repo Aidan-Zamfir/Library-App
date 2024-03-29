1d7b35f8d8757b8355709625c58967b1515305f4
Aidan-Z <aidanzamfir@gmail.com>
2024-03-29 12:08:08 -0300

    html base template

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '123'
+    app.config['SECRET_KEY'] = '123' # yes I know
 
     from .views import views
     from .auth import auth
