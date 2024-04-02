60ca860385391d7b771288e38ab8804617614b56
Aidan-Z <aidanzamfir@gmail.com>
2024-04-02 17:49:20 -0300

    library.html

--- website/views.py
+++ website/views.py
@@ -5,6 +5,10 @@ from flask_login import login_required, current_user
 views = Blueprint('views', __name__)
 
 @views.route('/')
-@login_required
 def home():
     return render_template("index.html")
+
+@views.route('/library')
+@login_required
+def library():
+    return render_template("library.html")
